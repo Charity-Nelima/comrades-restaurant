1f5822e7ba4233f77a450edfd02146af8a762a52
charity <wa.nelimacharity@gmail.com>
2022-12-05 18:10:01 +0300

    creating table review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_143255) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_150928) do
   create_table "menus", force: :cascade do |t|
     t.string "image"
     t.string "name"
@@ -35,6 +35,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_143255) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_restaurants_on_email", unique: true
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "description"
+    t.string "food_id"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
