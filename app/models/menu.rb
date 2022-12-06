56263ac47b8da92df10c83fd299f5fccf41e9495
luckyligovi <lucky.luvihi@student.moringaschool.com>
2022-12-06 09:05:15 +0300

    relations

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,6 @@
 class Menu < ApplicationRecord
+    belongs_to :Restaurant
+    belongs_to :user
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
+  belongs_to :user
+  has_one :Menu
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
